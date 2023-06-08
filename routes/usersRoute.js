ed279636eedb4ba4a6af5a699c8c45dda170c1d4
Riyaz-Github-Acc <riyazofficial0@gmail.com>
2023-06-08 16:37:16 +0530

    changes in verifyuser

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -10,13 +10,13 @@ import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 // UPDATE
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id", updateUser);
 
 // DELETE
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id", deleteUser);
 
 // GET
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", getUser);
 
 // GET ALL
 router.get("/", getAllUser);
